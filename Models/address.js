fca63999b9eef0d998452077af93e3d329e4bfaf
Yeabsira <4eabsira@gmail.com>
2021-12-20 18:13:37 -0600

    folder structure done

---
+++ Models/address.js
@@ -0,0 +1,9 @@
+module.exports = class Address{
+   constructor(city,state,zipCode,street)
+   {
+       this.city=city;
+       this.state=state;
+       this.zipCode =zipCode;
+       this.street=street;
+   }
+}
---
+++ Models/user.js
@@ -0,0 +1,12 @@
+module.exports = class User{
+    constructor(firstName,lastName,userName,password,email,phoneNumber) {
+        this.userId=userId;
+        this.firstName =firstName;
+        this.lastName=lastName;
+        this.userName=userName;
+        this.password=password;
+        this.email=email;
+        this.phoneNumber=phoneNumber;  
+     }
+
+};
